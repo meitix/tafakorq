30ed9f53fece1d8de2f7951d87881c2f28684b1c
Mahdi Valizadeh <M.valizadeh@live.com>
2018-09-16 15:31:44 +0430

    - file service handlers added.

--- src/services/file.service.js
+++ src/services/file.service.js
@@ -4,29 +4,29 @@ import { RNFetchBlob } from 'react-native-fetch-blob';
 import { urls } from './urls';
 export class FileService {
 
-//    static _authService = new AuthService();
-//     static dirs = RNFetchBlob.fs.dirs;
-//     static getFile = async (postId , fileId) => {
+   static _authService = new AuthService();
+    static dirs = RNFetchBlob.fs.dirs;
+    static getFile = async (postId , fileId) => {
 
-//        const userId = await this._authService.getUserId();
-//         const url = urls.getFile.concat(
-//             '?',
-//             'userId=',userId,
-//             '&',
-//             'PostId=',postId,
-//             '&',
-//             'fileId=',fileId
-//             );
+       const userId = await this._authService.getUserId();
+        const url = urls.getFile.concat(
+            '?',
+            'userId=',userId,
+            '&',
+            'PostId=',postId,
+            '&',
+            'fileId=',fileId
+            );
 
-//       return RNFetchBlob
-//        .config({
-//          fileCache : true,
-//          appendExt : 'tfkq'
-//        })
-//        .fetch('GET', url)
-//        .then((res) => {
-//         console.log('The file saved to ', res.path());
-//         LocalManager.saveToDevice('file_'.concat(fileId) , res.path()).then(() => LocalManager.saveToDevice('SavedFiles' , fileId))
-//        });
-    // }
+      return RNFetchBlob
+       .config({
+         fileCache : true,
+         appendExt : 'tfkq'
+       })
+       .fetch('GET', url)
+       .then((res) => {
+        console.log('The file saved to ', res.path());
+        LocalManager.saveToDevice('file_'.concat(fileId) , res.path()).then(() => LocalManager.saveToDevice('SavedFiles' , fileId))
+       });
+    }
 }
